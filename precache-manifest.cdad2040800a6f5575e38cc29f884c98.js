d882080696103d9056fbe4f3a6f94af8d212b90f
hockie2 <hockie2@yahoo.com.sg>
2024-12-04 12:30:34 +0800

    Updates

---
+++ precache-manifest.cdad2040800a6f5575e38cc29f884c98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b22372d57e2531bd0d77734bd874a70",
+    "url": "/picoed2/index.html"
+  },
+  {
+    "revision": "bb8da9ddd43c83595be4",
+    "url": "/picoed2/static/css/main.4947439d.chunk.css"
+  },
+  {
+    "revision": "488064d8cade00652238",
+    "url": "/picoed2/static/js/2.f8dbdae5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/picoed2/static/js/2.f8dbdae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb8da9ddd43c83595be4",
+    "url": "/picoed2/static/js/main.20f75897.chunk.js"
+  },
+  {
+    "revision": "303d40ce620a6eaabfe6",
+    "url": "/picoed2/static/js/runtime-main.d68a1336.js"
+  },
+  {
+    "revision": "e0a8e08c1dc612104fd18b22667fc1c2",
+    "url": "/picoed2/static/media/background_01.e0a8e08c.jpg"
+  }
+]);
