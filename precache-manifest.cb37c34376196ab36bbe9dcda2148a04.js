4c0443280931d15e30c960ab3d69266ed0c53087
hockie2 <hockie2@yahoo.com.sg>
2025-02-26 12:12:47 +0800

    Updates

---
+++ precache-manifest.cb37c34376196ab36bbe9dcda2148a04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafd12915cf4a0d14b666b68588cb87b",
+    "url": "/picoed2/index.html"
+  },
+  {
+    "revision": "16723be0879e1cbbee2d",
+    "url": "/picoed2/static/css/main.d6f1d24a.chunk.css"
+  },
+  {
+    "revision": "292a66ce00ecd4c02238",
+    "url": "/picoed2/static/js/2.3917e3ab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/picoed2/static/js/2.3917e3ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16723be0879e1cbbee2d",
+    "url": "/picoed2/static/js/main.a0e68839.chunk.js"
+  },
+  {
+    "revision": "303d40ce620a6eaabfe6",
+    "url": "/picoed2/static/js/runtime-main.d68a1336.js"
+  },
+  {
+    "revision": "3d51e23aa47dd911045cf3ca4cee7058",
+    "url": "/picoed2/static/media/background_01.3d51e23a.jpg"
+  },
+  {
+    "revision": "75dc0f2d7d6e635cb984f31e32f52028",
+    "url": "/picoed2/static/media/folks.75dc0f2d.png"
+  }
+]);
